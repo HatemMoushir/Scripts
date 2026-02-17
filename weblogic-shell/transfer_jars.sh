c680eeafdb762382a611710a74a5ade2adb19424
HatemMoushir <144990450+HatemMoushir@users.noreply.github.com>
2026-02-17 14:18:45 +0200

    Update and rename transfer_jar.sh to transfer_jars.sh

--- weblogic-shell/transfer_jars.sh
+++ weblogic-shell/transfer_jars.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
 # By Hatem Moushir 2026 v1.1
+
+NOW_DATE=$(date +"%y%m%d_%H%M")
+CUR_FLDR=/export/home/oracle/Company/tmp
+WORK_DIR=/export/home/oracle/Company
+REPORTS_DIR=$WORK_DIR/REPORTS
+VERSIONS_DIR=$WORK_DIR/VERSIONS/$NOW_DATE
+LOG_FILE=$WORK_DIR/logs/run_$NOW_DATE.log
+
 SRC_JARS="$WORK_DIR/SOURCES/jars"
-mkdir -p "$SRC_JARS" "$VERSIONS_DIR"
+
+mkdir -p "$SRC_JARS" "$VERSIONS_DIR" "$WORK_DIR/logs"
 
 found_jars=false
 
